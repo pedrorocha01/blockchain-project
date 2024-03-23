de2163d7129a04b6e4533563298eda392d01c4f8
Pedro Julio Rocha <pedrojulio_183@hotmail.com>
2024-03-23 10:14:20 -0300

    comentario

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ const blockchain = new BlockChain(Number(process.argv[2] || 4))
 const blockNumber = +process.argv[3] || 10
 let chain = blockchain.chain
 
+//criar leitor de arquivo xml
+
 for (let i = 1; i <= blockNumber; i++) {
   const block = blockchain.createBlock(`Block ${i}`)
   const mineInfo = blockchain.mineBlock(block)
