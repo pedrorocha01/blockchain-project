a39f7d5a33beb41c528614b154c4deabfaa2e143
Pedro Julio Rocha <pedrojulio_183@hotmail.com>
2024-05-16 09:25:52 -0300

    arquivo

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,17 @@ const blockchain = new BlockChain(Number(process.argv[2] || 4))
 const blockNumber = +process.argv[3] || 10
 let chain = blockchain.chain
 
+const readline = require("readline")
+const fs = require("fs")
+const line = readline.createInterface({
+  input: fs.createReadStream("C:\Users\pedro\OneDrive\Área de Trabalho\typescript-blockchain-main\src\arquivo.csv")
+});
+
+line.on( "line", (data: any) =>{
+  console.log(data);
+});
+
+
 for (let i = 1; i <= blockNumber; i++) {
   const block = blockchain.createBlock(`Block ${i}`)
   const mineInfo = blockchain.mineBlock(block)
